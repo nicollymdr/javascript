0551e52d4cc12f94d3daf7b4b60e3a5a76c3a89f
Nicolly Murgia dos Reis <134756022+nicollymdr@users.noreply.github.com>
2024-11-30 20:48:56 -0300

    ex13

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+//Estrutura de repetição com teste lógico no início
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//Estrutura de repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
