b7856c855869314867a53d009ae44623259f58aa
Nicolly Murgia dos Reis <134756022+nicollymdr@users.noreply.github.com>
2024-11-30 23:35:30 -0300

    ex16

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,6 @@
+var inicio = document.querySelector('id#inicio')
+var fim = document.querySelector('id#fim')
+var passo = document.querySelector('id#fim')
+var res = document.querySelector('id#res')
+
+for (var inicio;)
