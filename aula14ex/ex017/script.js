0be6dd2a5133c15d4e00dad82cd85fc9872a56b2
Nicolly Murgia dos Reis <134756022+nicollymdr@users.noreply.github.com>
2024-12-01 19:21:45 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -7,10 +7,13 @@ function tabuada() {
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
+        //cria um laço de repetição que vai de 1 até 10
         while (c <= 10) {
+            //cria um novo item da lista
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
+            //adiciona esse item à lista
             tab.appendChild(item)
             c++
         }
