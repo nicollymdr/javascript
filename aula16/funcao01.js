1833df3b48febd1fadf10d2963250227490508f0
Nicolly Murgia dos Reis <134756022+nicollymdr@users.noreply.github.com>
2024-12-04 14:45:34 -0300

    function

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+                  //parâmetro formal
+function parimpar(n){
+    if (n % 2 == 0) {    //ação
+        return 'Par'
+    } else {
+        return 'Ímpar' //retorno
+    }
+}
+            //chamada
+console.log(parimpar(223))
+                     //parâmetro real
